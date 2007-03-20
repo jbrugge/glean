54014525d3165388297158b063126f77141c9a9a
john <john@b8d075e3-c120-0410-8129-dabd5febe6cd>
2007-03-20 00:03:14 +0000

    Added other file types to publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 scp -P 232 doc/target/*.html jbrugge@jbrugge.com:httpdocs/glean
+scp -P 232 doc/target/*.css jbrugge@jbrugge.com:httpdocs/glean
+scp -P 232 doc/target/*.jpg jbrugge@jbrugge.com:httpdocs/glean
 
 scp -P 232 dist/glean-1.0b.zip jbrugge@jbrugge.com:httpdocs/glean
 
