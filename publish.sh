e1f804efafcfcfcab1fe2d4095604d21d7f0fb49
john <john@b8d075e3-c120-0410-8129-dabd5febe6cd>
2007-03-21 13:17:44 +0000

    Added packaging task to bundle as a tar.gz file

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ scp -P 232 doc/target/*.html jbrugge@jbrugge.com:httpdocs/glean
 scp -P 232 doc/target/*.css jbrugge@jbrugge.com:httpdocs/glean
 scp -P 232 doc/target/*.jpg jbrugge@jbrugge.com:httpdocs/glean
 
-scp -P 232 dist/glean-1.0b.zip jbrugge@jbrugge.com:httpdocs/glean
+scp -P 232 dist/* jbrugge@jbrugge.com:httpdocs/glean
 
