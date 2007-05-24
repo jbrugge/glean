1a6cde250a09fb8e7129aa7303ece4cb4301cd39
john <john@b8d075e3-c120-0410-8129-dabd5febe6cd>
2007-05-24 03:48:09 +0000

    Tweaked to let me pass in property settings when running the samples

--- run-samples.sh
+++ run-samples.sh
@@ -6,6 +6,7 @@ then
     SAMPLES="apache-ant commons-lang jdom spring struts-cookbook $JPS"
 else
     SAMPLES=$1
+    shift
 fi
 
 ANT_OPTS=-Xmx256M
@@ -15,5 +16,5 @@ for sample in $SAMPLES
 do
     echo "============ Running Glean over $sample code ==============="
     ant -Dfeedback.properties=sample/$sample.feedback.properties \
-        -Dgen.report.root=$HOME/Sites/$sample clean glean
+        -Dgen.report.root=$HOME/Sites/$sample $* clean glean
 done
