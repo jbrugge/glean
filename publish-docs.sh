ef769500c8708facecc46eaf6117fe9df20b3566
john <john@b8d075e3-c120-0410-8129-dabd5febe6cd>
2007-06-27 04:02:30 +0000

    Split shell scripts for publishing into docs and distrib versions

---
+++ publish-docs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scp -P 232 doc/target/*.html jbrugge@jbrugge.com:httpdocs/glean
+scp -P 232 doc/target/*.css jbrugge@jbrugge.com:httpdocs/glean
+scp -P 232 doc/target/*.jpg jbrugge@jbrugge.com:httpdocs/glean
+
--- publish.sh
+++ publish.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-scp -P 232 doc/target/*.html jbrugge@jbrugge.com:httpdocs/glean
-scp -P 232 doc/target/*.css jbrugge@jbrugge.com:httpdocs/glean
-scp -P 232 doc/target/*.jpg jbrugge@jbrugge.com:httpdocs/glean
-
 scp -P 232 dist/* jbrugge@jbrugge.com:httpdocs/glean
 
